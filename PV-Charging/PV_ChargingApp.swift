0a86348e2d1529e6e2c3728c64e8730ab196014c
Daniel Budusan <danibudusan@gmail.com>
2024-06-18 14:43:41 +0300

    Initial Commit

---
+++ PV-Charging/PV_ChargingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PV_ChargingApp.swift
+//  PV-Charging
+//
+//  Created by Daniel Budusan on 18.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct PV_ChargingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
